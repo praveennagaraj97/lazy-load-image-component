bb807c146721181ad323bd66dd4bf4de0804892e
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-05-24 12:52:22 +0530

    react implementation done

--- react/src/data.ts
+++ react/src/data.ts
@@ -1,10 +1,10 @@
 export const images = [
-  'https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-  'https://cdn.wallpapersafari.com/38/45/KftFVL.jpg',
-  'http://3.bp.blogspot.com/-joPDmLtiwCA/WD7XByK_lUI/AAAAAAAA8No/HfwCgFQuQJ4tDP_AIOK7sPPCoSjX2T3CACHM/s1600/tropical-sunrise-wallpaper-vorrei-essere-qui-pinterest.jpg',
-  'https://cdn.wallpapersafari.com/38/45/KftFVL.jpg',
-  'https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-  'https://cdn.wallpapersafari.com/38/45/KftFVL.jpg',
-  'http://3.bp.blogspot.com/-joPDmLtiwCA/WD7XByK_lUI/AAAAAAAA8No/HfwCgFQuQJ4tDP_AIOK7sPPCoSjX2T3CACHM/s1600/tropical-sunrise-wallpaper-vorrei-essere-qui-pinterest.jpg',
-  'https://cdn.wallpapersafari.com/38/45/KftFVL.jpg',
+  'https://images.unsplash.com/photo-1611362487355-757f215bb5ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1518811829466-1372392d4544?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80',
+  'https://images.unsplash.com/photo-1492078376836-73e36fbd55e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1614582832516-59aa97f70572?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1401&q=80',
+  'https://images.unsplash.com/photo-1621238705348-6ef939523bae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
+  'https://images.unsplash.com/photo-1621230479592-74eb315c1d22?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1620983379290-71e02d8ca563?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1505259839540-04105fcd8ba3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=685&q=80',
 ];
