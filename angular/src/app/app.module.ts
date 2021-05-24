e7a7f192b3a15d5dda96dc13183afc3780e0fd87
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-05-24 13:17:26 +0530

    angular implementation done

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AddonsComponent } from './addons/addons.component';
 import { AppComponent } from './app.component';
 import { LazyLoadedImageComponent } from './lazy-loaded-image/lazy-loaded-image.component';
 import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddonsComponent,
-    LazyLoadedImageComponent,
-    LoaderComponent,
-  ],
+  declarations: [AppComponent, LazyLoadedImageComponent, LoaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- angular/src/app/lazy-loaded-image/lazy-loaded-image.component.ts
+++ angular/src/app/lazy-loaded-image/lazy-loaded-image.component.ts
@@ -18,10 +18,21 @@ export class LazyLoadedImageComponent implements AfterViewInit {
   @Input() imageWidth: number;
   @Input() imageHeight: number;
   imageLoaded = false;
+  startLoading = false;
+
+  private observer: IntersectionObserver;
 
   @ViewChild('imageTag') imageElement: ElementRef;
+  @ViewChild('loadableref') loadableRef: ElementRef;
 
-  constructor(private renderer: Renderer2, private ngZone: NgZone) {}
+  constructor(private renderer: Renderer2, private ngZone: NgZone) {
+    this.observer = new IntersectionObserver(
+      this.intersectionObserverCallBack,
+      {
+        threshold: 0.7,
+      }
+    );
+  }
 
   ngAfterViewInit() {
     const { nativeElement } = this.imageElement;
@@ -29,12 +40,23 @@ export class LazyLoadedImageComponent implements AfterViewInit {
       this.imageLoaded = false;
       this.renderer.setStyle(nativeElement, 'display', 'none');
     }
+    this.observer.observe(this.loadableRef.nativeElement);
   }
 
   // Helpers
   imageLoadedEvent = () => {
-    this.ngZone.run(() => (this.imageLoaded = true));
+    this.ngZone.run(() => {
+      this.imageLoaded = true;
+    });
     this.imageLoaded = true;
     this.renderer.setStyle(this.imageElement.nativeElement, 'display', 'block');
+  };
+
+  private intersectionObserverCallBack = (
+    entries: IntersectionObserverEntry[]
+  ) => {
+    if (!this.startLoading && entries[0].isIntersecting) {
+      this.startLoading = true;
+    }
   };
 }
