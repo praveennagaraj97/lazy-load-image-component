9024584338d1c1b16b1ce8388847ba3c7bbef837
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-05-24 12:51:00 +0530

    revert

--- angular/src/app/addons/addons.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-addons',
-  templateUrl: './addons.component.html',
-  styleUrls: ['addons.component.scss'],
-})
-export class AddonsComponent {}
